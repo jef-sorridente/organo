765574f1a46484194b6017906812ea07d32f6974
jef-sorridente <jpadilha53@gmail.com>
2023-11-16 18:01:49 -0300

    useState do formulário

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -2,6 +2,7 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import { useState } from "react";
 
 export const Formulario = () => {
   const times = [
@@ -13,6 +14,10 @@ export const Formulario = () => {
     "Inovação e Gestão",
   ];
 
+  const [nome, setNome] = useState('');
+  const [cargo, setCargo] = useState('');
+  const [imagem, setImagem] = useState('');
+
   const aoSalvar = (evento) => {
     evento.preventDefault();
   };
@@ -21,9 +26,26 @@ export const Formulario = () => {
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite seu cargo"
+          valor={cargo}
+          aoAlterado={(valor) => setCargo(valor)}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
+        />
         <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
         <Botao>Criar Card</Botao>
       </form>
