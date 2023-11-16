8981d0f577be2c47ee29457f93c94cea1b922b46
Jeferson Soares <jpadilha53@gmail.com>
2023-11-16 16:27:44 -0300

    React: desenvolvendo com JavaScript - Organo

---
+++ src/componentes/Botao/Botao.js
@@ -0,0 +1,11 @@
+import "./Botao.css";
+
+export const Botao = (props) => {
+
+    return (
+        <button className="botao">
+            {props.children}
+        </button>
+    )
+
+}
---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,12 @@
+import "./CampoTexto.css"
+
+export const CampoTexto = (props) => {
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input required={props.obrigatorio} placeholder={props.placeholder}/>
+    </div>
+  );
+};
+
+export default CampoTexto;
